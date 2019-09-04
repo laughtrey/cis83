75226af8553a02fe4aa5a1e4e59f94824b724acb
Ray Laughrey <raymonl4963@student.vvc.edu>
2019-09-04 03:59:05 -0700

    Added first assignment, Update README.md

---
+++ 1 VariablesExpressions/main.py
@@ -0,0 +1,16 @@
+# File: main.py
+# Description: Variable project.
+# Author: Raymond Laughrey
+# Email: raymonl4963@student.vvc.edu
+# Date of creation: 09/04/2019
+import math
+x = 3
+y = 9
+print ("X is:", x, "Y is:", y)
+print("Sum:", x + y)
+print("Difference: ", x - y)
+print("Product: ", x * y)
+print("Average: ", (x + y) / 2)
+print("Distance: ", abs(x-y))
+print("Max: ", max(x,y))
+print("Min: ", min(x,y))
---
+++ README.md
@@ -0,0 +1,7 @@
+# CIS 83 - Python - zyBooks
+
+
+## Projects
+
+## 1 Variables and Expressions
+>Assignment operators and arithmetic operations in Python. Here we go again!
