91e5e5cf597a0f8046e201b8aaa05864b59880cf
Ray Laughrey <raymonl4963@student.vvc.edu>
2019-11-15 22:24:18 -0800

    Create main.py

---
+++ 11 Modules/main.py
@@ -0,0 +1,8 @@
+#File: main.py
+#Description: Dice Roll Module assignment
+#Author: Raymond Laughrey
+#Email: raymonl4963@student.vvc.edu
+#Date of Creation: Fri Nov 15 21:42:15 PDT 2019
+
+import dice
+dice.roll()
